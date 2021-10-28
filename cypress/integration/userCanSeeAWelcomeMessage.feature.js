9fab8f92a51b96b609b7f7ac58350b9dc414f585
Mljungho <mathias.ljungholm@gmail.com>
2021-10-28 21:21:17 +0200

    Yarn test work

--- cypress/integration/userCanSeeAWelcomeMessage.feature.js
+++ cypress/integration/userCanSeeAWelcomeMessage.feature.js
@@ -1,13 +1,13 @@
 describe("user that navigates to the application", ()=>{
  // step 2 INTERACT
     before(()=> {
-        crypto.visit('/')
+        cy.visit('/')
     })
 
     // Step 3 ASSERT
-    it('is expected to see "Hello World"') ()=>{
-        cy.get('h1').should('contain.text', "Hello Mars!")
-        cy.get('h3').should('contain.text', 'My first test')
+    it('is expected to see "Hello World"', ()=>{
+        cy.get('h1').should('contain.text', "Hello World!")
+        cy.get('h3').should('contain.text', 'My first test is going Green')
     })
 });
 
